911b424e5aed4a2e7721aedd896431d08c1fa42c
Łukasz Głowacki <lukasz.glowacki@ringieraxelspringer.pl>
2025-02-04 17:45:36 +0100

    dd

--- cloud-image-mirror-map.py
+++ cloud-image-mirror-map.py
@@ -7,9 +7,14 @@ from tabulate import tabulate
 with open("cloud-image-mirror-map.yaml", "r") as file:
     data = yaml.safe_load(file)
 
+
 # Prepare the table data
 table_data = []
+names = set()
 for image in data["images"]:
+    if image["name"] in names:
+        raise ValueError(f"Duplicate image name found: {image['name']}")
+    names.add(image["name"])
     for tag in image["tags"]:
         table_data.append([image["source"], image["name"], tag["name"]])
 
