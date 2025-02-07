ea38a030760a9f1eb329d33a0eb46d0a59952647
Łukasz Głowacki <lukasz.glowacki@ringieraxelspringer.pl>
2025-02-07 08:45:30 +0100

    install python fix

--- install-python.sh
+++ install-python.sh
@@ -8,45 +8,47 @@ TEMP="/tmp/install-python-temp"
 
 if [ ! -f "$TEMP" ]; then
     echo "Updating package list and installing dependencies..."
-    sudo apt update
-    sudo apt install -y build-essential libz-dev libreadline-dev libncursesw5-dev libssl-dev \
+    sudo apt update -q > /dev/null
+    sudo apt install -y -q build-essential libz-dev libreadline-dev libncursesw5-dev libssl-dev \
         libgdbm-dev libsqlite3-dev libbz2-dev lzma-dev liblzma-dev libzzip-dev libzip-dev \
         zlib1g zlib1g-dev libffi-dev libgdbm-dev tk-dev libncursesw5-dev
-
     touch "$TEMP"
 fi
-
-set -e
 
 PYTHON_VERSION="${1:-}"
 PYTHON_PACKAGE_VERSION="${2:-$PYTHON_VERSION}"
 PYTHON_PACKAGE="Python-${PYTHON_PACKAGE_VERSION}.tgz"
-echo $PYTHON_PACKAGE
+
+echo "
+PYTHON_VERSION: $PYTHON_VERSION
+PYTHON_PACKAGE: $PYTHON_PACKAGE
+"
+
+echo "Fetching available Python versions..."
+curl -sS https://www.python.org/ftp/python/ -o /tmp/python-lists-raw
+grep -oE '[0-9]+\.[0-9]+\.[0-9]+' /tmp/python-lists-raw | sort -V | uniq | grep ^3 > /tmp/python-lists
 
 if [ -z "$PYTHON_VERSION" ]; then
-    echo "Fetching available Python versions..."
-    curl -sS https://www.python.org/ftp/python/ -o /tmp/python-lists
-    grep -oE '[0-9]+\.[0-9]+\.[0-9]+' /tmp/python-lists | sort -V | uniq | grep ^3
+    cat /tmp/python-lists
     echo "Usage: $0 <python_version> [package_version]"
     exit 1
 fi
 
-
-
 if [[ ! "$PYTHON_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Error: Argument should be a valid Python version (e.g., 3.10.15)"
+    cat /tmp/python-lists
+    echo "Error: Argument should be a valid Python version (e.g., 3.12.9)"
     exit 1
 fi
 
 echo "Checking if Python $PYTHON_VERSION version exists..."
-curl -sSL https://www.python.org/ftp/python/${PYTHON_VERSION} -o /tmp/python-packages-raw
+curl -sSL -q https://www.python.org/ftp/python/${PYTHON_VERSION} -o /tmp/python-packages-raw
 cat /tmp/python-packages-raw | grep -Eo '>Python-.*.tgz' | sed 's/>//g' | sort | uniq > /tmp/python-packages
 
-set +e
-grep -q "$PYTHON_PACKAGE" /tmp/python-packages
-
-if [ "$?" -ne "0" ]; then
+echo ">>> Available Python versions:"
     cat /tmp/python-packages
+    echo "-------------------------------"
+
+if ! grep -q "$PYTHON_PACKAGE" /tmp/python-packages; then
     echo "Error: Python version ${PYTHON_PACKAGE} not found."
     exit 1
 fi
@@ -91,3 +93,11 @@ echo "Installing required Python packages..."
 /opt/python/${PYTHON_PACKAGE_VERSION}/bin/python3 -m pip install pyyaml
 
 echo "Python ${PYTHON_PACKAGE_VERSION} installation completed successfully."
+
+
+
+###
+echo "RUN TESTS .."
+# ls /opt/python/*/bin/python3 | sort | while read i; do echo -n "$i  " ; $i -m timeit -s "arr = list(range(10**6))" "sum(x**2 for x in arr)" ;  done
+# "import random; arr = [random.randint(1, 10**6) for _ in range(10**6)]" "sorted(arr)"
+#
